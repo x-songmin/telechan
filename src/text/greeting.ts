10fc4b0c3406092738eb2044af0c4c3037d81b74
x-songmin <68186221+x-songmin@users.noreply.github.com>
2022-09-08 12:56:57 +0800

    Update greeting.ts

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -7,7 +7,7 @@ const greeting = () => (ctx: any) => {
   const messageId = ctx.message.message_id;
   // const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
   
-  replyToMessage(ctx, messageId, `Welcome to TeleChan, type /sendkey to get your sendkey and api url`);
+  //replyToMessage(ctx, messageId, `Welcome to TeleChan, type /sendkey to get your sendkey and api url`);
   // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
 };
 
