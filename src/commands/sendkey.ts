68809813596b6bf6a9a14b1ef1154098aa587c5f
x-songmin <68186221+x-songmin@users.noreply.github.com>
2022-09-08 13:57:31 +0800

    Update sendkey.ts

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -7,13 +7,13 @@ const replyToMessage = (ctx: any, messageId: string, string: string) =>
     reply_to_message_id: messageId,
   });
 
-const sendkey = () => (ctx: any) => {
+// const sendkey = () => (ctx: any) => {
 
-  const messageId = ctx.message.message_id;
-  const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
-  const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
+//   const messageId = ctx.message.message_id;
+//   const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
+//   const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
 
-  const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
+//   const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
 
 //   replyToMessage(ctx, messageId, `${userName} , Your sendkey is 🔑 ${sendkey} \n 
 //   🚀 Use follow url to send message : \n 
