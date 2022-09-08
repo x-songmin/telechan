2e96afa8d1957917f5392aaec31c0d250fe6c270
x-songmin <68186221+x-songmin@users.noreply.github.com>
2022-09-08 13:58:28 +0800

    Update sendkey.ts

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -7,17 +7,17 @@ const replyToMessage = (ctx: any, messageId: string, string: string) =>
     reply_to_message_id: messageId,
   });
 
-// const sendkey = () => (ctx: any) => {
+const sendkey = () => (ctx: any) => {
 
-//   const messageId = ctx.message.message_id;
-//   const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
-//   const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
+  const messageId = ctx.message.message_id;
+  const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
+  const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
 
-//   const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
+  const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
 
-//   replyToMessage(ctx, messageId, `${userName} , Your sendkey is 🔑 ${sendkey} \n 
-//   🚀 Use follow url to send message : \n 
-//   ${site_url}/api/send?sendkey=<sendkey>&text=<text>`);
+  replyToMessage(ctx, messageId, `${userName} , Your sendkey is 🔑 ${sendkey} \n 
+  🚀 Use follow url to send message : \n 
+  ${site_url}/api/send?sendkey=<sendkey>&text=<text>`);
   // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
 };
 
