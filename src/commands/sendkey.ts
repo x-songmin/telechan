841794897a5254e7ddeec0dfa86731bcae268e77
x-songmin <68186221+x-songmin@users.noreply.github.com>
2022-09-08 13:56:11 +0800

    Update sendkey.ts

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -2,10 +2,10 @@ import md5 from 'md5';
 const VERCEL_URL = process.env.VERCEL_URL;
 const TCKEY = process.env.TCKEY;
 
-// const replyToMessage = (ctx: any, messageId: string, string: string) =>
-//   ctx.reply(string, {
-//     reply_to_message_id: messageId,
-//   });
+const replyToMessage = (ctx: any, messageId: string, string: string) =>
+  ctx.reply(string, {
+    reply_to_message_id: messageId,
+  });
 
 const sendkey = () => (ctx: any) => {
 
